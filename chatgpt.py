4e3935f4600f0986d8b30075a8a3c1a3828eecd8
Antonio Martinez <antonio_martinez88@hotmail.com>
2023-02-12 12:51:01 -0500

    updated

--- chatgpt.py
+++ chatgpt.py
@@ -1,7 +1,7 @@
 import os
 import openai
 
-openai.apikey = os.environ.get('OPENAI_KEY')
+openai.api_key = os.environ.get('OPENAI_API_KEY')
 
 while True:
     prompt = input("\n Introduce el prompt a chatgpt-3 o 'exit' para salir\n")
@@ -12,6 +12,6 @@ while True:
     completation = openai.Completion.create(engine="text-davinci-003",
                             prompt=prompt,
                             n=1,
-                            max_tokens="2048")
+                            max_tokens=2048)
 
     print(completation.choices[0].text)
